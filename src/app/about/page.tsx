84f3ee21b67f5476c2047c90ba6f1812a9dbca09
JuliaDarwin <juliaelguetaserra@gmail.com>
2026-03-28 11:02:15 +0100

    -

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -25,7 +25,8 @@ export default function About(){
             <div className="mb-30 text-center ">
                 <h2 className="m-4 mb-5 2xl:text-2xl">Start your learning here</h2>
                 <Link href="/lessons">
-                    <button className="inline-flex items-center rounded-full bg-amber-400 px-6 py-3 text-lg 2xl:text-3xl font-semibold text-slate-950 shadow-sm hover:bg-amber-300 transition-colors">
+                    <button 
+                    className="inline-flex items-center rounded-full bg-amber-400 px-6 py-3 text-lg 2xl:text-3xl font-semibold text-slate-950 shadow-sm hover:bg-amber-300 transition-colors">
                         Get started
                     </button>                
                 </Link>
