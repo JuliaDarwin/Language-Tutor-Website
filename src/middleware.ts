4db92ceb5a54ec0215da8ddef14850cd10beb61b
JuliaDarwin <juliaelguetaserra@gmail.com>
2026-04-11 16:30:17 +0200

    fixed Middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,27 +6,27 @@ the goal here is to protect routes and enforce authentication
 aka if someone not admin tries to go to admin pahge, or someone not signed in tries a not public page, 
 they should be redirected to homepage
 */
-const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)", "/about(.*)", "/contact(.*)", "/lessons(.*)"]);
+const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)", "/about(.*)", "/contact(.*)", "/lessons(.*)", "/api/webhooks(.*)"]);
 
 //we create the protected admin route
 const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
-    //if(!isPublicRoute(req)) await auth.protect();
+  //if(!isPublicRoute(req)) await auth.protect();
 
-    const { userId, redirectToSignIn } = await auth();
-    //here says if the route is an admin route but the user is not admin, we redirect to homepage
-    if(
-        isAdminRoute(req) &&
-        (await auth()).sessionClaims?.metadata?.role !== "admin"
-    ) {
-        const url = new URL("/", req.url);
-        return NextResponse.redirect(url);
-    }
-    //if the route is not public and user is not signed in
-    if(!userId && !isPublicRoute(req)) {
-        return redirectToSignIn();
-    }
+  const { userId, redirectToSignIn } = await auth();
+  //here says if the route is an admin route but the user is not admin, we redirect to homepage
+  if (
+    isAdminRoute(req) &&
+    (await auth()).sessionClaims?.metadata?.role !== "admin"
+  ) {
+    const url = new URL("/", req.url);
+    return NextResponse.redirect(url);
+  }
+  //if the route is not public and user is not signed in
+  if (!userId && !isPublicRoute(req)) {
+    return redirectToSignIn();
+  }
 });
 
 export const config = {
